c8425e22755db3a0f89cc3e8f33a201679ebc64c
ireneiw <109014121+ireneiw@users.noreply.github.com>
2022-07-30 16:10:03 -0700

    js class for kill copy

---
+++ noCopy.js
@@ -0,0 +1,15 @@
+‹script type="text/Javascript">
+//courtesy of BoogieJack.com
+function killcopy(e){
+    return false
+}
+function reEnable){
+    return true
+}
+document.onselectstart=new Function ("return false")
+
+if (window.sidebar){
+    document. onmousedown=killcopy
+    document.onclick=reEnable
+}
+</script>
